6383c937818869341b602a69d74817fa9cc8573f
theoriginalcaliforniaburrito <47760827+theoriginalcaliforniaburrito@users.noreply.github.com>
2019-05-10 13:43:41 -0700

    added stylings

--- presidentialClient/presidential-client/src/components/home.js
+++ presidentialClient/presidential-client/src/components/home.js
@@ -30,7 +30,7 @@ class Home extends Component {
     render() {
         return (
             <div className="container-fluid">
-                <div id="hero-header" className="row">
+                <div className="row">
                     <div className="jumbotron">
                         <h1>Presidential API</h1>
                         <p className="lead">Data on the Presidents of the USA</p>
@@ -40,36 +40,38 @@ class Home extends Component {
                     <p>Search Presidents with different API calls</p>
                     <p><b>Search by name, number in the presidency, etc.</b></p>
                     <p>lorem ipsum <b>:)</b>dolor sit amet</p>
-                    <img src={this.state.result.portrait} alt=""/>
+                    <img id="presidential-portrait" src={this.state.result.portrait} className="rounded-circle mx-auto d-block" alt=""/>
                 </div>
                 <div className="row">
                     <div className="col-lg-8 col-md-8 col-sm-8">
-                        <h1 className="center">Try it now!</h1>
+                        <h1 className="center">Make an API call!</h1>
                         <div className="input-group">
-                            <span className="input-group-addon">localhost5000</span>
+                            <span className="input-group-addon">http://localhost:5000/presidents/</span>
                             <input onChange={(e) => this.setState({ search: e.target.value })}
-                                type="text" className="form-control" placeholder="presidents/1/" />
+                                type="text" className="form-control" placeholder="number/1/" />
                             <span className="input-group-btn">
                                 <button onClick={(e) => this.getPresident(e)}
                                     className="btn btn-primary">search presidents</button>
                             </span>
                         </div>
                         <div className="row">
-                            <small>Hint, try: <a href="#">president/number</a><a href="#">president/name</a><a href="#">president/party</a></small>
+                            <small>Try these calls: <a href="#">number/1</a>, <a href="#">name/George%20Washington</a>, <a href="#">420/blazeit</a></small>
                         </div>
                         <div className="row">
                             <p className="lead pad_top">Result:</p>
                             <div className="well">
                                 <pre className="pre-scrollable">
-                                    <p>President {this.state.result.number} of the United States of America</p>
-                                    <p>{this.state.result.name} ({this.state.result.born} - {this.state.result.died})</p>
-                                    <p>In Office {this.state.result.start} - {this.state.result.end}</p>
-                                    <p>Number of terms served: {this.state.result.terms}</p>
-                                    <p>{this.state.result.party}</p>
-                                    <p>Vice President: {this.state.result.vice}</p>
-                                    <p>Prior profession: {this.state.result.prior}</p>
-                                    <p>From {this.state.result.home}, {this.state.result.state}</p>
-                                    <p>Married to {this.state.result.spouse}</p>
+                                    {/* <Highlight className='javascript'> */}
+                                        <p>President {this.state.result.number} of the United States of America</p>
+                                        <p>{this.state.result.name} ({this.state.result.born} - {this.state.result.died})</p>
+                                        <p>In Office {this.state.result.start} - {this.state.result.end}</p>
+                                        <p>Number of terms served: {this.state.result.terms}</p>
+                                        <p>{this.state.result.party}</p>
+                                        <p>Vice President: {this.state.result.vice}</p>
+                                        <p>Prior profession: {this.state.result.prior}</p>
+                                        <p>From {this.state.result.home}, {this.state.result.state}</p>
+                                        <p>Married to {this.state.result.spouse}</p>
+                                    {/* </Highlight> */}
                                 </pre>
                             </div>
                         </div>
