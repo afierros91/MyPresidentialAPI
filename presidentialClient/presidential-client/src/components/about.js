c3d28ad553540c4bd0739b7b8c3b2e5fc6a571f8
theoriginalcaliforniaburrito <47760827+theoriginalcaliforniaburrito@users.noreply.github.com>
2019-05-16 13:09:21 -0700

    added about info

--- presidentialClient/presidential-client/src/components/about.js
+++ presidentialClient/presidential-client/src/components/about.js
@@ -3,23 +3,31 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return(
-            <div className="container">
-                <div className="row">
+            <div className="flex-container">
+                <div id="about" className="row">
                     <div className="col-md-6">
                         <h2>Introducing the Presidential API</h2>
-                        <p>lorem ipsum dolor sit amet</p>
-                        <p>lorem ipsum dolor sit amet</p>
-                        <p>lorem ipsum dolor sit amet</p>
-                        <h2>What can you do with this?</h2>
-                        <p>lorem ipsum dolor sit amet</p>
+                        <p>This is a library of presidential data.</p>
+                        <p>Presidents can currently be filtered by party, state of birth, and number of terms.</p>
+                        <p>Data can also be called on individual Presidents by name and number in the presidency.</p>
+
+                        <h2>What is this for?</h2>
+                        <p>Use Presidential API to make API calls of your own.</p>
                         <p><em>lorem ipsum dolor sit amet</em></p>
                         <pre>
                             <code>
                                 code goes here
                             </code>
                         </pre>
-                        <h2>wow</h2>
-                        <p>list technologies here</p>
+
+                        <h2>What was this built with?</h2>
+                        <p>Presidential API was created using Node.JS, MongoDB, Bootstrap, and React.</p>
+
+                        <h2>Contributors</h2>
+                            <ul>
+                                <li><a href="https://github.com/theoriginalcaliforniaburrito" target="blank">Sebastian Chu</a></li>
+                                <li><a href="https://github.com/afierros91" target="blank">Anthony Fierros</a></li>
+                            </ul>
                     </div>
                 </div>
             </div>
